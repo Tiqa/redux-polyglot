19c40232615b2c895c8094917a71f905d05c1152
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-10-04 18:26:30 +0200

    selectors : tests OK.

---
+++ src/selectors.spec.js
@@ -0,0 +1,40 @@
+import { getP, getLocale } from './selectors';
+
+const fakeState = {
+    polyglot: {
+        locale: 'fr',
+        phrases: { hello: 'bonjour' },
+    },
+};
+
+describe('selectors', () => {
+    describe('getLocale', () => {
+        it('should not crash when state is an empty object', () => {
+            expect(getLocale({})).toBe(undefined);
+        });
+
+        it('should not crash when state is an empty object', () => {
+            expect(getLocale(fakeState)).toBe('fr');
+        });
+    });
+
+    describe('getP', () => {
+        const p = getP(fakeState);
+
+        it('should not crash when state is an empty object', () => {
+            expect(getP({})).toBe(undefined);
+        });
+
+        it('should translate "hello" to "bonjour"', () => {
+            expect(p.t('hello')).toBe('bonjour');
+        });
+
+        it('should translate "hello" to "Bonjour" (capitalize)', () => {
+            expect(p.tc('hello')).toBe('Bonjour');
+        });
+
+        it('should translate "hello" to "BONJOUR" (upper-case)', () => {
+            expect(p.tu('hello')).toBe('BONJOUR');
+        });
+    });
+});
