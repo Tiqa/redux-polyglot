0bdcceb5f2a59df827a2b8873e01459ff75e4182
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-10-04 15:16:38 +0200

    middleware : OK.

--- src/middleware.js
+++ src/middleware.js
@@ -1,1 +1,17 @@
-export const polyglotMiddleware = () => 42;
+import { setLanguage } from './actions';
+
+export const polyglotMiddleware = (catchedActionType, getLocale, getPhrases) => {
+    if (!catchedActionType || !getLocale || !getPhrases)
+        throw (new Error('polyglotMiddleware : missing parameters.'));
+    return store => next => action => {
+        if (catchedActionType === action.type) {
+            const locale = getLocale(action);
+            const previousLocale = store.getState().polyglot.locale;
+            if (previousLocale !== locale) {
+                const phrases = getPhrases(locale);
+                store.dispatch(setLanguage(locale, phrases));
+            }
+        }
+        return next(action);
+    };
+};
