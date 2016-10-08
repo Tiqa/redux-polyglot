afae18ced400fd06d506462f72036ae2b4b2d8b3
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-10-08 23:16:32 +0200

    translate.spec : cover displayName tests.

--- src/translate.spec.js
+++ src/translate.spec.js
@@ -11,24 +11,46 @@ const fakeStore = createStore(createRootReducer(), {
     polyglot: { locale: 'en', phrases: { hello: 'hello' } },
 });
 
-const DummyComponent = ({ p }) => (
-    <div
-        data-t={p.t('hello')}
-        data-tc={p.tc('hello')}
-        data-tu={p.tu('hello')}
-    />
-);
+const createAnonymousComponent = () => () => (<div />);
+
+const DummyComponentWithDisplayName = createAnonymousComponent();
+const name = 'DummyComponent';
+DummyComponentWithDisplayName.displayName = name;
 
 describe('translate enhancer', () => {
+    const DummyComponent = ({ p }) => (
+        <div
+            data-t={p.t('hello')}
+            data-tc={p.tc('hello')}
+            data-tu={p.tu('hello')}
+        />
+    );
+    const EnhancedComponent = translate(DummyComponent);
+
+    const tree = renderer.create(
+        <Provider store={fakeStore}>
+            <EnhancedComponent />
+        </Provider>
+    ).toJSON();
+
     it('should provide a valid p object.', () => {
-        const EnhancedComponent = translate(DummyComponent);
-        const tree = renderer.create(
-            <Provider store={fakeStore}>
-                <EnhancedComponent />
-            </Provider>
-        ).toJSON();
         expect(tree.props['data-t']).toBe('hello');
         expect(tree.props['data-tc']).toBe('Hello');
         expect(tree.props['data-tu']).toBe('HELLO');
+    });
+
+    describe('displayName', () => {
+        it('should have a valid displayName', () => {
+            const translatedName = `Translated(${name})`;
+            expect(EnhancedComponent.displayName).toBe(translatedName);
+            expect(translate(DummyComponentWithDisplayName).displayName)
+                .toBe(translatedName);
+        });
+
+        it('shoudld have a default name when it is an anonymous component', () => {
+            const translatedDefaultName = 'Translated(Component)';
+            const TranslatedComponent = translate(createAnonymousComponent());
+            expect(TranslatedComponent.displayName).toBe(translatedDefaultName);
+        });
     });
 });
