a110872804bfe9e89effcded70681909e9cf9222
Jalil Arfaoui <jalil@arfaoui.net>
2016-10-09 10:06:13 +0200

    Reworded some tests

--- src/selectors.spec.js
+++ src/selectors.spec.js
@@ -10,11 +10,11 @@ const fakeState = {
 
 describe('selectors', () => {
     describe('getLocale', () => {
-        it('should not crash when state is an empty object', () => {
+        it('doesn\'t crash when state is an empty object', () => {
             expect(getLocale({})).toBe(undefined);
         });
 
-        it('should not crash when state is an empty object', () => {
+        it('doesn\'t crash when state is an empty object', () => {
             expect(getLocale(fakeState)).toBe('fr');
         });
     });
@@ -22,23 +22,23 @@ describe('selectors', () => {
     describe('getP', () => {
         const p = getP(fakeState);
 
-        it('should not crash when state is an empty object', () => {
+        it('doesn\'t crash when state is an empty object', () => {
             expect(getP({})).toBe(undefined);
         });
 
-        it('should translate "hello" to "bonjour"', () => {
+        it('translates "hello" to "bonjour"', () => {
             expect(p.t('hello')).toBe('bonjour');
         });
 
-        it('should translate "hello" to "Bonjour" (capitalize)', () => {
+        it('translates "hello" to "Bonjour" (capitalize)', () => {
             expect(p.tc('hello')).toBe('Bonjour');
         });
 
-        it('should translate "hello" to "BONJOUR" (upper-case)', () => {
+        it('translates "hello" to "BONJOUR" (upper-case)', () => {
             expect(p.tu('hello')).toBe('BONJOUR');
         });
 
-        it('should translate "hello" to "BONJOUR" (morphed with upper-case function)', () => {
+        it('translates "hello" to "BONJOUR" (morphed with upper-case function)', () => {
             expect(p.tm(toUpper)('hello')).toBe('BONJOUR');
         });
     });
