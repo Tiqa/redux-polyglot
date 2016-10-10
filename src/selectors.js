479fdcfa7b05af4f4824a5e8303ec978ba97a797
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-10-10 16:18:11 +0200

    getP() selector, can take an optional parameter for setting polyglotScope.

--- src/selectors.js
+++ src/selectors.js
@@ -2,6 +2,9 @@ import { compose } from 'redux';
 import { createSelector } from 'reselect';
 import Polyglot from 'node-polyglot';
 
+// eslint-disable-next-line valid-typeof
+const is = type => x => typeof x === type;
+const isString = is('string');
 const path = arrPath => obj => arrPath.reduce((cursor, key) => cursor && cursor[key], obj);
 const toUpper = str => str.toUpperCase();
 const adjustString = (f, index) => str => (
@@ -11,37 +14,45 @@ const capitalize = adjustString(toUpper, 0);
 
 const getLocale = path(['polyglot', 'locale']);
 const getPhrases = path(['polyglot', 'phrases']);
+const getPolyglotScope = (state, options) => (
+    options.polyglotScope === '' ? '' : `${options.polyglotScope}.`
+);
 
 const getPolyglot = createSelector(
     getLocale,
     getPhrases,
-    (locale, phrases) => {
+    getPolyglotScope,
+    (locale, phrases, polyglotScope = '') => {
         const p = new Polyglot({
             locale,
             phrases,
         });
+        const t = p.t.bind(p);
+        p.t = (text, ...args) => t(polyglotScope + text, ...args);
         return p;
     }
 );
 
 const getTranslation = createSelector(
     getPolyglot,
-    p => p.t.bind(p)
+    (p) => p.t.bind(p)
 );
 
-const getTranslationMorphed = state => f => compose(f, getTranslation(state));
-const getTranslationUpperCased = state => getTranslationMorphed(state)(toUpper);
-const getTranslationCapitalized = state => getTranslationMorphed(state)(capitalize);
+const getTranslationMorphed = (...args) => f => compose(f, getTranslation(...args));
+const getTranslationUpperCased = (...args) => getTranslationMorphed(...args)(toUpper);
+const getTranslationCapitalized = (...args) => getTranslationMorphed(...args)(capitalize);
 
-const getP = state => {
+const getP = (state, options = {}) => {
     if (!getLocale(state) || !getPhrases(state))
         return undefined;
+    const polyglotScope = isString(options) ? options : options.polyglotScope;
+    const ownProps = { polyglotScope: polyglotScope || '' };
     return {
-        ...getPolyglot(state),
-        t: getTranslation(state),
-        tc: getTranslationCapitalized(state),
-        tu: getTranslationUpperCased(state),
-        tm: getTranslationMorphed(state),
+        ...getPolyglot(state, ownProps),
+        t: getTranslation(state, ownProps),
+        tc: getTranslationCapitalized(state, ownProps),
+        tu: getTranslationUpperCased(state, ownProps),
+        tm: getTranslationMorphed(state, ownProps),
     };
 };
 
