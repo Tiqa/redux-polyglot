4e62733164e53839da00fe1291b5127d0be11c78
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-10-10 23:24:22 +0200

    translate : curried form.

--- src/translate.js
+++ src/translate.js
@@ -1,15 +1,32 @@
+import curry from 'lodash.curry';
 import { connect } from 'react-redux';
 import { getP } from './selectors';
 
-const getDisplayName = WrappedComponent => (
-    WrappedComponent.displayName || WrappedComponent.name || 'Component'
+// eslint-disable-next-line valid-typeof
+const is = type => x => typeof x === type;
+const isString = is('string');
+const isFunction = is('function');
+
+const getDisplayName = Component => (
+    Component.displayName || Component.name || 'Component'
 );
 
-const mapPolyglotToProps = options => state => ({ p: getP(state, options) });
-const translate = (Component, options) => {
-    const Connected = connect(mapPolyglotToProps(options))(Component);
+const mapPolyglotToProps = polyglotScope => state => ({
+    p: getP(state, polyglotScope),
+});
+
+const translateEnhancer = curry((polyglotScope, Component) => {
+    const Connected = connect(mapPolyglotToProps(polyglotScope))(Component);
     Connected.displayName = `Translated(${getDisplayName(Connected.WrappedComponent)})`;
     return Connected;
+});
+
+const translate = (fstArg, sndArg) => {
+    if (isFunction(fstArg))
+        return translateEnhancer('', fstArg);
+    else if (isString(fstArg) && sndArg === undefined)
+        return translateEnhancer(fstArg);
+    return translateEnhancer(fstArg, sndArg);
 };
 
 export default translate;
