26796d2da170b3cf4ab0af066dead19ec975bc71
Nimi Wariboko Jr <nimi@channelmeter.com>
2017-01-25 19:10:23 -0800

    Export shape PropType for p

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+import { PropTypes } from 'react';
+
+const PropType = PropTypes.shape({
+    t: PropTypes.func.isRequired,
+    tc: PropTypes.func.isRequired,
+    tt: PropTypes.func.isRequired,
+    tu: PropTypes.func.isRequired,
+    tm: PropTypes.func.isRequired,
+});
+
 export * from './middleware';
 export * from './actions';
 export * from './constants';
 export * from './reducer';
 export * from './selectors';
+export { PropType };
