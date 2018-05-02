b9543611abf99c97ad098e36e29eb40c50aa15c2
Guillaume ARM <trapcodien@gmail.com>
2018-05-02 11:57:18 +0200

    fix(package): update all dependencies
    deps, devDeps and peerDeps

--- src/private/utils.js
+++ src/private/utils.js
@@ -5,4 +5,4 @@ export const is = type => x => typeof x === type;
 export const isString = is('string');
 export const isFunction = is('function');
 export const isObject = is('object');
-export const isArray = Array.isArray;
+export const { isArray } = Array;
